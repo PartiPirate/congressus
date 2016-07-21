7179e338a9bfeb5a60ed725290976b3522974fd7
farlistener <contact@levieuxcedric.com>
2016-07-21 10:36:40 +0200

    # i18n

--- application/administration.php
+++ application/administration.php
@@ -42,7 +42,7 @@ include_once("header.php");
 			<div class="panel-body panel-collapse collapse in" id="server-panel-body">
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="server_base_input">Base d'url</label>
+					<label class="col-md-2 control-label" for="server_base_input"><?php echo lang("administration_server_base"); ?></label>
 					<div class="col-md-10">
 						<input id="server_base_input" name="server_base_input" type="text" value="<?php echo $config["server"]["base"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -50,12 +50,12 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="server_line_input">Ligne du serveur</label>
+					<label class="col-md-2 control-label" for="server_line_input"><?php echo lang("administration_server_line"); ?></label>
 					<div class="col-md-4">
 						<select id="server_line_input" name="server_line_input" class="form-control">
-							<option value="dev"  <?php if ("dev"  == $config["server"]["line"]) echo "selected"; ?>>Developpement</option>
-							<option value="beta" <?php if ("beta" == $config["server"]["line"]) echo "selected"; ?>>Beta</option>
-							<option value=""     <?php if (""     == $config["server"]["line"]) echo "selected"; ?>>Production</option>
+							<option value="dev"  <?php if ("dev"  == $config["server"]["line"]) echo "selected"; ?>><?php echo lang("administration_server_line_dev"); ?></option>
+							<option value="beta" <?php if ("beta" == $config["server"]["line"]) echo "selected"; ?>><?php echo lang("administration_server_line_beta"); ?></option>
+							<option value=""     <?php if (""     == $config["server"]["line"]) echo "selected"; ?>><?php echo lang("administration_server_line_prod"); ?></option>
 						</select>
 					</div>
 				</div>
@@ -70,12 +70,12 @@ include_once("header.php");
 			<div class="panel-body panel-collapse collapse " id="database-panel-body">
 			
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="database_host_input">Hôte BDD</label>
+					<label class="col-md-2 control-label" for="database_host_input"><?php echo lang("administration_database_host"); ?></label>
 					<div class="col-md-4">
 						<input id="database_host_input" name="database_host_input" type="text" value="<?php echo $config["database"]["host"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="database_port_input">Port BDD</label>
+					<label class="col-md-2 control-label" for="database_port_input"><?php echo lang("administration_database_port"); ?></label>
 					<div class="col-md-4">
 						<input id="database_port_input" name="database_port_input" type="text" value="<?php echo $config["database"]["port"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -83,7 +83,7 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="database_database_input">BDD</label>
+					<label class="col-md-2 control-label" for="database_database_input"><?php echo lang("administration_database_database"); ?></label>
 					<div class="col-md-10">
 						<input id="database_database_input" name="database_database_input" type="text" value="<?php echo $config["database"]["database"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -91,12 +91,12 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="database_login_input">Login BDD</label>
+					<label class="col-md-2 control-label" for="database_login_input"><?php echo lang("administration_database_login"); ?></label>
 					<div class="col-md-4">
 						<input id="database_login_input" name="database_login_input" type="text" value="<?php echo $config["database"]["login"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="database_password_input">Password BDD</label>
+					<label class="col-md-2 control-label" for="database_password_input"><?php echo lang("administration_database_password"); ?></label>
 					<div class="col-md-4">
 						<input id="database_password_input" name="database_password_input" type="text" value="<?php echo $config["database"]["password"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -104,7 +104,7 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="galette_db_input">Galette BDD</label>
+					<label class="col-md-2 control-label" for="galette_db_input"><?php echo lang("administration_database_galette"); ?></label>
 					<div class="col-md-10">
 						<input id="galette_db_input" name="galette_db_input" type="text" value="<?php echo $config["galette"]["db"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -112,7 +112,7 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="personae_db_input">Personae BDD</label>
+					<label class="col-md-2 control-label" for="personae_db_input"><?php echo lang("administration_database_personae"); ?></label>
 					<div class="col-md-10">
 						<input id="personae_db_input" name="personae_db_input" type="text" value="<?php echo $config["personae"]["db"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -129,12 +129,12 @@ include_once("header.php");
 			<div class="panel-body panel-collapse collapse " id="memcached-panel-body">
 			
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="memcached_host_input">Hôte Memcached</label>
+					<label class="col-md-2 control-label" for="memcached_host_input"><?php echo lang("administration_memcached_host"); ?></label>
 					<div class="col-md-4">
 						<input id="memcached_host_input" name="memcached_host_input" type="text" value="<?php echo $config["memcached"]["host"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="memcached_port_input">Port Memcached</label>
+					<label class="col-md-2 control-label" for="memcached_port_input"><?php echo lang("administration_memcached_port"); ?></label>
 					<div class="col-md-4">
 						<input id="memcached_port_input" name="memcached_port_input" type="text" value="<?php echo $config["memcached"]["port"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -151,12 +151,12 @@ include_once("header.php");
 			<div class="panel-body panel-collapse collapse " id="mail-panel-body">
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="smtp_host_input">Hôte SMTP</label>
+					<label class="col-md-2 control-label" for="smtp_host_input"><?php echo lang("administration_mail_host"); ?></label>
 					<div class="col-md-4">
 						<input id="smtp_host_input" name="smtp_host_input" type="text" value="<?php echo $config["smtp"]["host"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="smtp_port_input">Port SMTP</label>
+					<label class="col-md-2 control-label" for="smtp_port_input"><?php echo lang("administration_mail_port"); ?></label>
 					<div class="col-md-4">
 						<input id="smtp_port_input" name="smtp_port_input" type="text" value="<?php echo $config["smtp"]["port"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -164,10 +164,10 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="smtp_secure_input">Sécurisation Mail</label>
+					<label class="col-md-2 control-label" for="smtp_secure_input"><?php echo lang("administration_mail_secure"); ?></label>
 					<div class="col-md-4">
 						<select id="smtp_secure_input" name="smtp_secure_input" class="form-control">
-							<option value=""    <?php if ("" ==    $config["smtp"]["secure"]) echo "selected"; ?>>Aucune</option>
+							<option value=""    <?php if ("" ==    $config["smtp"]["secure"]) echo "selected"; ?>><?php echo lang("administration_mail_secure_none"); ?></option>
 							<option value="ssl" <?php if ("ssl" == $config["smtp"]["secure"]) echo "selected"; ?>>SSL</option>
 							<option value="tls" <?php if ("tls" == $config["smtp"]["secure"]) echo "selected"; ?>>TLS</option>
 						</select>
@@ -175,12 +175,12 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="smtp_username_input">Username SMTP</label>
+					<label class="col-md-2 control-label" for="smtp_username_input"><?php echo lang("administration_mail_username"); ?></label>
 					<div class="col-md-4">
 						<input id="smtp_username_input" name="smtp_username_input" type="text" value="<?php echo $config["smtp"]["username"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="smtp_password_input">Password SMTP</label>
+					<label class="col-md-2 control-label" for="smtp_password_input"><?php echo lang("administration_mail_password"); ?></label>
 					<div class="col-md-4">
 						<input id="smtp_password_input" name="smtp_password_input" type="text" value="<?php echo $config["smtp"]["password"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -188,12 +188,12 @@ include_once("header.php");
 				</div>
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="smtp_from_address_input">Adresse From</label>
+					<label class="col-md-2 control-label" for="smtp_from_address_input"><?php echo lang("administration_mail_from_address"); ?></label>
 					<div class="col-md-4">
 						<input id="smtp_from_address_input" name="smtp_from_address_input" type="text" value="<?php echo $config["smtp"]["from.address"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="smtp_from_name_input">Nom From</label>
+					<label class="col-md-2 control-label" for="smtp_from_name_input"><?php echo lang("administration_mail_from_name"); ?></label>
 					<div class="col-md-4">
 						<input id="smtp_from_name_input" name="smtp_from_name_input" type="text" value="<?php echo $config["smtp"]["from.name"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
@@ -210,12 +210,12 @@ include_once("header.php");
 			<div class="panel-body panel-collapse collapse " id="account-panel-body">
 
 				<div class="form-group">
-					<label class="col-md-2 control-label" for="administrator_login_input">Login Administrateur</label>
+					<label class="col-md-2 control-label" for="administrator_login_input"><?php echo lang("administration_account_login"); ?></label>
 					<div class="col-md-4">
 						<input id="administrator_login_input" name="administrator_login_input" type="text" value="<?php echo $config["administrator"]["login"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->
 					</div>
-					<label class="col-md-2 control-label" for="administrator_password_input">Password Administrateur</label>
+					<label class="col-md-2 control-label" for="administrator_password_input"><?php echo lang("administration_account_password"); ?></label>
 					<div class="col-md-4">
 						<input id="administrator_password_input" name="administrator_password_input" type="text" value="<?php echo $config["administrator"]["password"] ?>" placeholder="placeholder" class="form-control input-md"> 
 						<!-- <span class="help-block">help</span> -->

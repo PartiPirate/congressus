2bf104f89df4833fdb8923533cd2a06bfe698253
Nino TREYSSAT-VINCENT <treyssatvincent@protonmail.com>
2017-07-07 11:58:44 +0200

    bug fix

--- application/export_pdf.php
+++ application/export_pdf.php
@@ -8,7 +8,7 @@
         <a class="btn btn-default navbar-btn" href="meeting/do_export.php?template=pdf&id=<?php echo $meeting["mee_id"]; ?>" target="_blank"><?php echo lang("export_open"); ?></a>
       </div>
       <div class="navbar-header pull-right">
-        <a title="<?php echo $meeting["common_close"]; ?>" class="btn btn-default navbar-btn exportClose" href="#"><span class="glyphicon glyphicon-remove"></span></a>
+        <a title="<?php echo lang("common_close"); ?>" class="btn btn-default navbar-btn exportClose" href="#"><span class="glyphicon glyphicon-remove"></span></a>
       </div>
     </div>
     <iframe id="iframe_pdf" src=""><?php echo lang("export_iframes"); ?></iframe>

e2b78ec35b2235634f7a35ee2d27802ad5e89f6a
Nino TREYSSAT-VINCENT <treyssatvincent@protonmail.com>
2017-07-10 09:53:04 +0200

    responsive fix

--- application/export_html.php
+++ application/export_html.php
@@ -24,7 +24,7 @@
       </div>
       <div class="navbar-header pull-left">
         <a id="rendering" data-format="html" class="btnShowExport hidden-xs btn btn-default navbar-btn btn-active" href="#"><?php echo lang("export_rendering"); ?></a>
-        <a id="html-code" data-format="html-code" class="btnShowExport btn btn-default navbar-btn" href="#">Code HTML</a>
+        <a id="html-code" data-format="html-code" class="btnShowExport hidden-xs btn btn-default navbar-btn" href="#">Code HTML</a>
         <a id="newpage" class="btn btn-default navbar-btn" href="meeting/do_export.php?template=html&id=<?php echo $meeting["mee_id"]; ?>?textarea=false" target="_blank"><?php echo lang("export_open"); ?></a>
       </div>
       <div class="navbar-header pull-right">

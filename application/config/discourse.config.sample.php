59a0c5ec5df6f2672a2637e995657bf2a758b81f
Cédric Levieux <farlistener@users.noreply.github.com>
2017-07-18 19:22:46 +0000

    #68 upgrade a bit the discourse call

---
+++ application/config/discourse.config.sample.php
@@ -0,0 +1,10 @@
+<?php 
+
+// TODO: Add a configuration pannel for the administrator to edit theses values.
+$allowed_categories = array( // Add here the categories allowed for export.
+  // "Ektek",
+  // "CR - CN",
+  //"Sandbox"
+);
+
+?>

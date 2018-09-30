7afeffa6351ca4aaff07646da3d1e561ffefa972
langlais115 <langlais115@yahoo.fr>
2018-09-30 22:10:26 +0200

    Improve english translation

--- application/language/en/admin.php
+++ application/language/en/admin.php
@@ -17,47 +17,72 @@
     along with Congressus.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-$lang["administration_guide"] = "Gérez ici les informations de gestion de l'application";
-$lang["administration_server"] = "Server informations";
-$lang["administration_server_base"] = "Base url";
-$lang["administration_server_line"] = "Server line";
-$lang["administration_server_line_dev"] = "Développement";
-$lang["administration_server_line_beta"] = "Beta";
-$lang["administration_server_line_prod"] = "Production";
-$lang["administration_server_timezone"] = "Timezone";
-$lang["administration_server_timezone_none"] = "None";
+$lang["administration_guide"] = "Application Settings";
+
+$lang["administration_server"]                      = "Server informations";
+$lang["administration_server_base"]                 = "Base url";
+$lang["administration_server_line"]                 = "Server line";
+$lang["administration_server_line_dev"]             = "Developpement";
+$lang["administration_server_line_beta"]            = "Beta";
+$lang["administration_server_line_prod"]            = "Production";
+$lang["administration_server_timezone"]             = "Timezone";
+$lang["administration_server_timezone_none"]        = "None";
 $lang["administration_server_timezone_europeparis"] = "Europe/Paris";
-$lang["administration_database"] = "Information database";
-$lang["administration_database_host"] = "Database host";
-$lang["administration_database_port"] = "Database port";
-$lang["administration_database_database"] = "Database";
-$lang["administration_database_login"] = "Login Database";
-$lang["administration_database_password"] = "Password Database";
-$lang["administration_database_galette"] = "Galette Database";
-$lang["administration_database_personae"] = "Personae Database";
-$lang["administration_memcached"] = "Memcached information ";
-$lang["administration_memcached_host"] = "Memcached host";
-$lang["administration_memcached_port"] = "Memcached port";
-$lang["administration_mail"] = "Mail Information";
-$lang["administration_mail_host"] = "SMTP Host";
-$lang["administration_mail_port"] = "SMTP port";
-$lang["administration_mail_secure"] = "Mail securing";
-$lang["administration_mail_secure_none"] = "None";
-$lang["administration_mail_secure_none_alert"] = "Mails are sent in plain and could be intercepted";
-$lang["administration_mail_secure_ssl_alert"] = "Mails are sent encrypted but may be intercepted";
-$lang["administration_mail_secure_tls_alert"] = "Mails are sent encrypted";
-$lang["administration_mail_username"] = "Username SMTP";
-$lang["administration_mail_password"] = "Password SMTP";
-$lang["administration_mail_from_address"] = "Adresse From";
-$lang["administration_mail_from_name"] = "Name From";
-$lang["administration_account"] = "Administrator's login";
-$lang["administration_account_login"] = "Login Administrator";
-$lang["administration_account_password"] = "Password Administrator";
-$lang["administration_ping_database"] = "Test";
-$lang["administration_alert_ok"] = "The configuration has been updated";
-$lang["administration_alert_ping_ok"] = "The database's configuration is good";
-$lang["administration_alert_ping_no_host"] = "Unknown host";
-$lang["administration_alert_ping_bad_credentials"] = "Bad account";
-$lang["administration_alert_ping_no_database"] = "Database nonexistent";
+
+$lang["administration_database"]            = "Database settings";
+$lang["administration_database_host"]       = "Database host";
+$lang["administration_database_port"]       = "Database port";
+$lang["administration_database_database"]   = "Database";
+$lang["administration_database_login"]      = "Login Database";
+$lang["administration_database_password"]   = "Password Database";
+$lang["administration_database_galette"]    = "Galette Database";
+$lang["administration_database_personae"]   = "Personae Database";
+
+$lang["administration_memcached"]       = "Memcached settings";
+$lang["administration_memcached_host"]  = "Memcached host";
+$lang["administration_memcached_port"]  = "Memcached port";
+
+$lang["administration_mail"]                    = "Mail settings";
+$lang["administration_mail_host"]               = "SMTP Host";
+$lang["administration_mail_port"]               = "SMTP port";
+$lang["administration_mail_secure"]             = "Mail securing";
+$lang["administration_mail_secure_none"]        = "None";
+$lang["administration_mail_secure_none_alert"]  = "Mails are sent in plain and could be intercepted";
+$lang["administration_mail_secure_ssl_alert"]   = "Mails are sent encrypted but may be intercepted";
+$lang["administration_mail_secure_tls_alert"]   = "Mails are sent encrypted";
+$lang["administration_mail_username"]           = "Username SMTP";
+$lang["administration_mail_password"]           = "Password SMTP";
+$lang["administration_mail_from_address"]       = "Adresse From";
+$lang["administration_mail_from_name"]          = "Name From";
+
+$lang["administration_account"]             = "Administrator's login";
+$lang["administration_account_login"]       = "Login Administrator";
+$lang["administration_account_password"]    = "Password Administrator";
+
+$lang["administration_ping_database"]       = "Test";
+
+$lang["administration_alert_ok"]                        = "The configuration has been updated";
+$lang["administration_alert_ping_ok"]                   = "The database's configuration is good";
+$lang["administration_alert_ping_no_host"]              = "Unknown host";
+$lang["administration_alert_ping_bad_credentials"]      = "Bad credentials";
+$lang["administration_alert_ping_no_database"]          = "Unknow database";
+$lang["administration_congressus_ballot_majorities"]    = "Modalité de vote";
+
+$lang["administration_discourse"]           = "Discourse";
+$lang["administration_discourse_api_key"]   = "API Key";
+$lang["administration_discourse_url"]       = "Host";
+$lang["administration_discourse_protocol"]  = "Protocol";
+$lang["administration_discourse_user"]      = "Impersonification";
+$lang["administration_discourse_base"]      = "Discourse base URL";
+$lang["administration_discourse_allowed_categories"] = "Allowed categoties";
+
+$lang["administration_mediawiki"]               = "Mediawiki"; 
+$lang["administration_mediawiki_url"]           = "Host";
+$lang["administration_mediawiki_login"]         = "Login";
+$lang["administration_mediawiki_password"]      = "Password";
+$lang["administration_mediawiki_base"]          = "Mediawiki base URL";
+$lang["administration_mediawiki_categories"]    = "Catégories connues";
+
+?>
 
 ?>

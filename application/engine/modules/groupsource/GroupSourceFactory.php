43f536265b2849ccaa4c5380bc0e4b4a299f4676
farlistener <contact@levieuxcedric.com>
2018-10-17 18:55:25 +0000

    Solve a problem

--- application/engine/modules/groupsource/GroupSourceFactory.php
+++ application/engine/modules/groupsource/GroupSourceFactory.php
@@ -46,7 +46,10 @@ class GroupSourceFactory {
 		$people["mem_speaking"] = $ping["pin_speaking"];
 		$people["mem_speaking_request"] = $ping["pin_speaking_request"];
 		$people["mem_speaking_time"] = $ping["pin_speaking_time"];
-		$people["mem_current_speaking_time"] = $ping["pin_current_speaking_time"];
+		
+		if (isset($ping["pin_current_speaking_time"])) {
+			$people["mem_current_speaking_time"] = $ping["pin_current_speaking_time"];
+		}
     }
 
     static function fixPing(&$pings, &$usedPings, &$people, $member, $now) {
